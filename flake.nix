d33f38e25df367a5376f2772763143f92e6884b9
Sanjaikumar <sanjaikumarbca@gmail.com>
2025-03-06 20:35:32 +0530

    migrate to flakes

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
+
